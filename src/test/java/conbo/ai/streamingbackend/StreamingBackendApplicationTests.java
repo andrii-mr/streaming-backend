283d45f200246cd480e656de447c296270a60c4d
Andrey Marchenko <a.marchenko@owox.com>
2025-02-27 05:41:52 +0100

    create skeleton of project

---
+++ src/test/java/conbo/ai/streamingbackend/StreamingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package conbo.ai.streamingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
